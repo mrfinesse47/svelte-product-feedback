15de6376a9d901fb845991d46aa47310bc93819d
Kevin Mason <krmason2020@gmail.com>
2022-06-17 18:39:05 -0700

    added in some assets icons etc.

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Header from "./components/Header.svelte";
+</script>
+
+<main>
+  <Header />
+</main>
+
+<!-- we are going to use global style variables this time -->
+<style>
+  :global(:root) {
+    --header-color: purple;
+  }
+  main {
+    width: 23.4375rem;
+    margin: 0 auto;
+    /* temporary for mobile dev first */
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<h1>Hello</h1>
+
+<style>
+  h1 {
+    color: var(--header-color);
+    background-color: aqua;
+  }
+</style>
