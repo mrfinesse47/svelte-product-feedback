844b36be51788c8354d35d467ceae490c4aa4ae4
Kevin Mason <krmason2020@gmail.com>
2022-06-17 19:02:50 -0700

    laying out header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,37 @@
-<h1>Hello</h1>
+<header>
+  <div class="title">
+    <h1>Hello</h1>
+    <h3>Feedback Board</h3>
+  </div>
+  <div class="hamburger-menu">
+    <img src="./assets/shared/mobile/icon-hamburger.svg" alt="open menu" />
+  </div>
+</header>
 
 <style>
+  header {
+    background-image: url("../assets/suggestions/mobile/background-header.png");
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
   h1 {
-    color: var(--header-color);
-    background-color: aqua;
+    font-size: 15px;
+    line-height: 22px;
+    letter-spacing: -0.1875px;
+    color: #ffffff;
+  }
+  h3 {
+    font-weight: 500;
+    font-size: 13px;
+    line-height: 19px;
+    color: #ffffff;
+    mix-blend-mode: normal;
+    opacity: 0.75;
+  }
+  .title {
+  }
+  .hamburger-menu {
+    align-self: center;
   }
 </style>
