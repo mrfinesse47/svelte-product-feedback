05de1c1d3c3d951ba28942a7d06365f0fe8d0586
Kevin Mason <krmason2020@gmail.com>
2022-07-14 18:08:50 -0700

    updates to product feedback card

--- src/components/ProductRequests/ProductRequest.svelte
+++ src/components/ProductRequests/ProductRequest.svelte
@@ -5,22 +5,42 @@
 </script>
 
 <li>
-  <header>Add tags for Solutions</header>
-  <p>Easier to search for solutions based on a specific stack</p>
-  <Button noClick>test</Button>
-  <footer>
-    <UpVoteButton count={112} />
-    <CommentCounter count={100} />
-  </footer>
+  <div class="container">
+    <header>Add tags for Solutions</header>
+    <p>Easier to search for solutions based on a specific stack</p>
+    <Button noClick>Enhancment</Button>
+    <footer>
+      <UpVoteButton count={112} />
+      <CommentCounter count={2} />
+    </footer>
+  </div>
 </li>
 
 <style>
+  header {
+    font-size: 0.8125rem;
+    font-weight: 700;
+    line-height: 1.1875rem;
+    letter-spacing: -0.0113rem;
+  }
+  p {
+    color: var(--grayish-blue);
+    font-weight: 400;
+    font-size: 0.8125rem;
+    line-height: 1.1875rem;
+    margin-bottom: 0.5rem;
+    margin-top: 0.5625rem;
+  }
+  .container {
+    padding: 24px;
+  }
   li {
     background-color: white;
     width: 20.4375rem;
     margin: 1rem auto;
   }
   footer {
+    margin-top: 1rem;
     display: flex;
     justify-content: space-between;
     align-items: center;
--- src/shared/CommentCounter.svelte
+++ src/shared/CommentCounter.svelte
@@ -3,12 +3,20 @@
   export let count;
 </script>
 
-<div><Comment />{count}</div>
+<div><Comment /><span>{count}</span></div>
 
 <style>
   div {
     display: flex;
     align-items: center;
     color: var(--dark-text);
+  }
+  span {
+    padding-left: 0.25rem;
+    font-weight: 700;
+    font-size: 0.8125rem;
+    line-height: 1.1875rem;
+    text-align: center;
+    letter-spacing: -0.0113rem;
   }
 </style>
